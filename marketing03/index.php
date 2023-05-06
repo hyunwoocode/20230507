37e073a364e5f1cacf915d3c4d7179703a829277
hyunwoocode <132482755+hyunwoocode@users.noreply.github.com>
2023-05-07 01:26:59 +0900

    Add files via upload

---
+++ marketing03/index.php
@@ -0,0 +1,295 @@
+<!doctype html>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=0.45,minimum-scale=0.45,maximum-scale=0.45,user-scalable=no">
+<meta http-equiv="imagetoolbar" content="no">
+<meta http-equiv="X-UA-Compatible" content="IE=10,chrome=1">
+<title>BMMOST-MARKETING</title>
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<style type='text/css'>
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+
+* {margin:0;padding:0;}
+body {margin:0;padding:0;font-size:0;font-family: 'Pretendard-Regular',sans-serif;}
+a {text-decoration:none;}
+img {border:0;}
+ul,li {margin:0;padding:0;list-style:none;}
+.wrap {width:100%;margin:0 auto;overflow:hidden;max-width:1920px;padding-bottom:250px;}
+.img_wr{position:Relative;}
+.tit{text-align:center;}
+
+.video_wr{position:relative;background:#ed592b;padding:180px 0 160px;}
+.video_wr:before{content:"";position:absolute;top:0;left:50%;margin-left:-1px;width:2px;height:125px;background:#fff;}
+.video_wr .video{width:1400px;margin:70px auto 0;}
+.video_wr .video ul:after{content:"";display:block;clear:both;}
+.video_wr .video ul li{float:left;position:relative;margin-right:18px;margin-top:20px;cursor:pointer;}
+.video_wr .video ul li:nth-child(4n){margin-right:0;}
+.video_wr .video ul li:before{content:"";position:absolute;top:0;left:0;display:block;width:100%;height:100%;background:rgba(0,0,0,0.8);transition:.5s;opacity:0;}
+.video_wr .video ul li:after{content:"▶";position:absolute;top:50%;left:50%;margin-top:-25px;margin-left:-25px;display:block;width:50px;height:50px;border:1px solid #fff;border-radius:50%;font-size:15px;color:#fff;text-align:center;line-height:50px;transition:.5s;opacity:0;}
+.video_wr .video ul li:hover:before{opacity:1}
+.video_wr .video ul li:hover:after{opacity:1}
+.video_bg{position:fixed;top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,0.9);z-index:9;display:none;}
+.video_pop{position:fixed;top:20%;left:50%;margin-left:-400px;background:#000;z-index:10;display:none;}
+.video_pop iframe{width:800px;height:450px;}
+.video_pop .video_close{position:absolute;bottom:-40px;width:100%;font-size:25px;color:#fff;text-align:center;cursor:pointer;}
+
+.partner_wr{position:Relative;background:#171717;padding:155px 0;}
+.partner{width:1400px;margin:80px auto 0;}
+.partner ul:after{content:"";display:block;clear:both;}
+.partner ul li{float:left;width:200px;height:104px;border:1px solid #303030;margin-right:32px;margin-top:14px;text-align:center;cursor:pointer;}
+.partner ul li:nth-child(6n){margin-right:0;}
+.partner ul li img{position:relative;top:50%;transform:translateY(-50%);}
+
+.event5box {position:fixed;width:100%;max-width:1920px;height:250px;left:0;bottom:0;background:#222;padding-top:70px;box-sizing:border-box;}
+.event5wrap {position:relative;width:1200px;left:50%;margin-left:-700px;}
+.event5wrap .bm_logo{position:absolute;top:10px;left:30px;}
+.submit {position:relative;width:100%;text-align:Center;border-spacing:0;}
+.submit tr{display:inline-block;margin:5px 20px;font-size:0;}
+.submit th{display:inline-block;height:40px;line-height:40px;font-size:20px;vertical-align:middle;padding:0;color:#fff;font-weight:400;text-align:left;}
+.submit td{display:inline-block;width:330px;height:40px;}
+.submit td input,.submit td select{width:100%;height:100%;line-height:40px;border:none;font-size:20px;padding-left:10px;box-sizing:border-box;}
+
+.submit tr.info_wr{width:100%;}
+.submit .info {width:100%;color:#dadada;font-size:16px;text-align:left;padding-left:270px;}
+.submit .info input{position:Relative;top:2px;width:16px;height:16px;border:1px solid #dadada;}
+.submit .info label{color:#dadada;font-size:16px;}
+
+.input_btn{position:absolute;top:5px;right:0;margin-right:-145px;width:260px;height:90px;background:#ff6633;line-height:90px;font-size:20px;color:#fff;text-align:center;cursor:pointer;border:none;border-radius:10px;font-weight:600;}
+
+.agree-view{margin-top:-330px !important;}
+
+</style>
+
+
+
+</head>
+<body>
+
+<script type="text/javascript" language="JavaScript">
+
+           var mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android', 'Windows CE', 'Windows CE;', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson', 'Mobile', 'Symbian', 'Opera Mobi', 'Opera Mini', 'IEmobile');
+           for (var word in mobileKeyWords){
+               if (navigator.userAgent.match(mobileKeyWords[word]) != null){
+                         window.location.href = "/landing/marketing03/mindex.php";
+                   break;
+                     }
+           }
+
+</script>
+
+
+
+
+
+
+
+
+
+
+<div class="wrap">
+	<div class="img_wr"><img src="img/img01.jpg" alt=""></div>
+	<div class="video_bg"></div>
+	<div class="img_wr video_wr">
+		<div class="tit"><img src="img/img02.png" alt=""></div>
+		<div class="video">
+			<ul>
+				<li><img src="img/video01.jpg" alt=""></li>
+				<li><img src="img/video02.jpg" alt=""></li>
+				<li><img src="img/video03.jpg" alt=""></li>
+				<li><img src="img/video04.jpg" alt=""></li>
+				<li><img src="img/video05.jpg" alt=""></li>
+				<li><img src="img/video06.jpg" alt=""></li>
+				<li><img src="img/video07.jpg" alt=""></li>
+				<li><img src="img/video08.jpg" alt=""></li>
+			</ul>
+		</div>
+		<div class="video_pop">
+			<p class="video_close">CLOSE</p>
+			<iframe src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+	</div>
+	<div class="img_wr"><img src="img/img03.jpg" alt=""></div>
+	<div class="img_wr partner_wr">
+		<div class="tit"><img src="img/img04.png" alt=""></div>
+		<div class="partner">
+			<ul>
+				<li><img src="img/logo01.png" alt=""></li>
+				<li><img src="img/logo02.png" alt=""></li>
+				<li><img src="img/logo03.png" alt=""></li>
+				<li><img src="img/logo04.png" alt=""></li>
+				<li><img src="img/logo05.png" alt=""></li>
+				<li><img src="img/logo06.png" alt=""></li>
+				<li><img src="img/logo07.png" alt=""></li>
+				<li><img src="img/logo08.png" alt=""></li>
+				<li><img src="img/logo09.png" alt=""></li>
+				<li><img src="img/logo10.png" alt=""></li>
+				<li><img src="img/logo11.png" alt=""></li>
+				<li><img src="img/logo12.png" alt=""></li>
+				<li><img src="img/logo13.png" alt=""></li>
+				<li><img src="img/logo14.png" alt=""></li>
+				<li><img src="img/logo15.png" alt=""></li>
+				<li><img src="img/logo16.png" alt=""></li>
+				<li><img src="img/logo17.png" alt=""></li>
+				<li><img src="img/logo18.png" alt=""></li>
+				<li><img src="img/logo19.png" alt=""></li>
+				<li><img src="img/logo20.png" alt=""></li>
+				<li><img src="img/logo21.png" alt=""></li>
+				<li><img src="img/logo22.png" alt=""></li>
+				<li><img src="img/logo23.png" alt=""></li>
+				<li><img src="img/logo24.png" alt=""></li>
+				<li><img src="img/logo25.png" alt=""></li>
+				<li><img src="img/logo26.png" alt=""></li>
+				<li><img src="img/logo27.png" alt=""></li>
+				<li><img src="img/logo28.png" alt=""></li>
+				<li><img src="img/logo29.png" alt=""></li>
+				<li><img src="img/logo30.png" alt=""></li>
+				<li><img src="img/logo31.png" alt=""></li>
+				<li><img src="img/logo32.png" alt=""></li>
+				<li><img src="img/logo33.png" alt=""></li>
+				<li><img src="img/logo34.png" alt=""></li>
+				<li><img src="img/logo35.png" alt=""></li>
+				<li><img src="img/logo36.png" alt=""></li>
+			</ul>
+		</div>
+	</div>
+	<div class="event5box">
+		<div class="event5wrap">
+			<div class="bm_logo"><img src="img/ft_logo.png" alt=""></div>
+			<form name="fwrite" id="fwrite" onsubmit="return submit1(this);" method="post" enctype="multipart/form-data" autocomplete="off">
+			<input type="hidden" name="w" value="<?php echo $w ?>">
+			<input type="hidden" name="qa_id" value="<?php echo $qa_id ?>">
+			<input type="hidden" name="sca" value="<?php echo $sca ?>">
+			<input type="hidden" name="stx" value="<?php echo $stx ?>">
+			<input type="hidden" name="page" value="<?php echo $page ?>">
+			<input type="hidden" name="qa_18" value="의료업">
+			<input type="hidden" name='qa_category' value="마케팅">
+			<input type='hidden' name='qa_12' value='랜딩'>
+			<input type='hidden' name='lancheck' value='1'>
+			<input type='hidden' name='qa_16' value='<?php echo $_SERVER[HTTP_REFERER];?>'>
+			<input type='hidden' name='lan_chk' value='<?php echo $_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI];?>'>
+			<table class='submit' cellspacing='0'>
+				<tr>
+					<th width="140">업체명[병원명]</th>
+					<td><input type='text' class="inputr" title="업체명" name="qa_subject" id='qa_subject'></td>
+				</tr>
+				<tr>
+					<th width="55">예산</th>
+					<td><input type='text' class="inputr" title="예산" name="qa_4" id='qa_4'></td>
+				</tr>
+				<tr>
+					<th width="140">연락처</th>
+					<td><input type='text' class="inputr" title="연락처" name="qa_hp" placeholder="- 없이 입력해주세요." id='qa_hp'></td>
+				</tr>
+				<tr>
+					<th width="55">내용</th>
+					<td><input type='text' class="inputr" title="내용" name="qa_content" id='qa_content'></td>
+				</tr>				
+				<tr class="info_wr">
+					<td class="info" colspan="2"><input type="checkbox" id='agree' value="" checked /><label for='agree'> 개인정보취급방침에 동의합니다.</label></td>
+				</tr>
+			</table>
+			<input type="button" value="상담신청 바로가기" onclick='submit1();' class="input_btn" />
+		</div>
+	</div>
+</div>
+
+<script>
+$(function(){
+	// youtube
+	var video = [];
+	video[0] = 'https://www.youtube.com/embed/RzbhMf3XwNs'
+	video[1] = 'https://www.youtube.com/embed/JhQ88QO3MU4'
+	video[2] = 'https://www.youtube.com/embed/wRAq1QOrJOE'
+	video[3] = 'https://www.youtube.com/embed/sWrfJzAQ0Oo'
+	video[4] = 'https://www.youtube.com/embed/dKrC5Ns4VdA'
+	video[5] = 'https://www.youtube.com/embed/PCAIqwC3KTw'
+	video[6] = 'https://www.youtube.com/embed/2yuCN9qWQY4'
+	video[7] = 'https://www.youtube.com/embed/NX1C0Rsr1qE'
+	$('.video ul li').click(function(){
+		var n = $(this).index();
+		$('.video_bg').show();
+		$('.video_pop').show();
+		$('.video_pop').children('iframe').attr('src',video[n]+'?controls=1&autoplay=1');
+	});
+	$('.video_close').click(function(){
+		$('.video_bg').hide();
+		$('.video_pop').hide();
+		$('.video_pop').children('iframe').attr('src','');
+	});
+	
+	// partner
+	$('.partner ul li').mouseover(function(){
+		var src = $(this).children('img').attr('src');
+		var str = src.substr(4, 6);
+		var ov = $(this).children('img').attr('src','img/'+str+'_ov.png');
+	});
+	$('.partner ul li').mouseleave(function(){
+		var src = $(this).children('img').attr('src');
+		var str = src.substr(4, 6);
+		var leave = $(this).children('img').attr('src','img/'+str+'.png');
+	});
+});
+</script>
+
+
+<script>
+function submit1()
+{
+
+	form = document.fwrite;
+
+
+	if(form.qa_subject.value=="")
+	{
+		alert("업체명을 입력해주세요.");
+		form.qa_subject.focus();
+		return;
+	}
+	if(form.qa_4.value=="")
+	{
+		alert("예산을 입력해주세요.");
+		form.qa_4.focus();
+		return;
+	}
+
+	if(form.qa_hp.value=="")
+	{
+		alert("연락처를 입력해주세요.");
+		form.qa_hp.focus();
+		return;
+	}
+
+	if(form.qa_content.value=="")
+	{
+		alert("문의내용을 입력해주세요.");
+		form.qa_content.focus();
+		return;
+	}
+
+	 if (!form.agree.checked)
+	{
+		alert("개인정보취급방침에 동의하셔야됩니다.");
+		form.agree.focus();
+        return; 
+	}
+
+	form.action ='http://bmmost.net/bbs/qawrite_update.php';
+	form.submit();
+}
+</script>
+
+
+
+</body>
+</html>
+
+
+
+
---
+++ marketing03/mindex.php
@@ -0,0 +1,308 @@
+<!doctype html>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=720,user-scalable=no">
+<meta http-equiv="imagetoolbar" content="no">
+<meta http-equiv="X-UA-Compatible" content="IE=10,chrome=1">
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.js"></script>
+<title>BMMOST-MARKETING</title>
+<style type='text/css'>
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+
+* {vertical-align:middle;margin:0;padding:0;}
+body {margin:0;padding:0;font-family:'Pretendard-Regular', sans-serif}
+a {text-decoration:none;}
+img {border:0;}
+ul,li {margin:0;padding:0;list-style:none;}
+.wrap {width:100%;max-width:720px;margin:0 auto;overflow:hidden;}
+.img_wr{position:Relative;}
+/* .img_wr img{width:100%;} */
+input,select{-webkit-border-radius:0;}
+.tit{text-align:center;}
+
+.video_wr{position:relative;background:#ed592b;padding:100px 0}
+.video_wr:before{content:"";position:absolute;top:0;left:50%;margin-left:-1px;width:2px;height:60px;background:#fff;}
+.video_wr .video{position:Relative;width:100%;margin:50px 0 80px 50px;}
+.video_wr .video .swiper-slide{width:520px;height:270px;margin-left:40px}
+.video_wr .video .swiper-slide img{width:100%}
+.video_wr .swiper-counter{position:absolute;top:50%;left:50%;margin-left:-140px;margin-top:220px;font-size:28px;font-weight:300;line-height:35px;color:#fff;}
+.video_wr .swiper-counter span{position:relative;color:#ffcc99;font-size:30px;}
+.video_wr .swiper-counter span i{position: relative;top: -4px;margin-left:8px;margin-right:10px;font-size: 24px;}
+
+.video_wr .swiper-button-next, .video_wr .swiper-button-prev{position:absolute;margin-top:210px;width:53px;height:53px;z-index:1;}
+.video_wr .swiper-button-next{right:auto;left:50%;margin-left:70px;background:url('img/arrow_next.png');}
+.video_wr .swiper-button-prev{left:50%;background:url('img/arrow_prev.png');}
+
+.video_bg{position:fixed;top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,0.9);z-index:9;display:none;}
+.video_pop{position:fixed;top:20%;left:50%;margin-left:-300px;background:#000;z-index:10;display:none;}
+.video_pop iframe{width:600px;height:350px;}
+.video_pop .video_close{position:absolute;bottom:-60px;width:100%;font-size:35px;color:#fff;text-align:center;cursor:pointer;}
+
+.partner_wr{position:Relative;background:#171717;padding:100px 0;}
+.partner{width:650px;margin:70px auto 0;}
+.partner ul:after{content:"";display:block;clear:both;}
+.partner ul li{float:left;width:200px;height:104px;border:1px solid #303030;margin-right:22px;margin-top:14px;text-align:center;cursor:pointer;}
+.partner ul li:nth-child(3n){margin-right:0;}
+.partner ul li img{position:relative;top:50%;transform:translateY(-50%);}
+.partner_more{position:relative;margin-top:70px;width:100%;height:90px;background:#ff6633;line-height:90px;font-size:30px;color:#fff;text-align:center;cursor:pointer;border:none;border-radius:10px;font-weight:400;}
+
+.event5box {position:relative;left:0;bottom:0;width:100%;padding:100px 0 ;background:#222;margin:0 auto;}
+.event5wrap {position:relative;width:90%;left:5%;margin-top:40px;}
+
+.submit {position:relative;width:100%;text-align:Center;border-spacing:0;}
+.submit tr{display:block;margin-top:25px;font-size:0;}
+.submit th{display:inline-block;width:30%;height:65px;line-height:65px;font-size:25px;padding:0;color:#fff;font-weight:400;letter-spacing:-1px;text-align:left;vertical-align:top:}
+.submit td{display:inline-block;width:70%;height:65px;vertical-align:top !important;}
+.submit td input,.submit td select{width:100%;height:100%;line-height:65px;border:none;font-size:25px;padding-left:10px;box-sizing:border-box;background-color:#fff;}
+
+.submit .info {width:100%;color:#dadada;font-size:23px;text-align:right;}
+.submit .info input{position:Relative;top:1px;width:24px;height:24px;border:1px solid #dadada;}
+.submit .info input:checked{background:#fff;}
+.submit .info label{color:#dadada;font-size:23px;}
+.submit .info a {color:#dadada;}
+.input_btn{position:relative;margin-top:30px;width:100%;height:90px;background:#ff6633;line-height:90px;font-size:30px;color:#fff;text-align:center;cursor:pointer;border:none;border-radius:10px;font-weight:600;}
+
+.agree-btn{font-size:20px !important;}
+.agree-view{width:600px !important; left:10px !important; margin-left:0px !important; margin-top:-540px !important;}
+.agree-view p{font-size:20px !important;line-height:1.8 !important;}
+.agree-view .close-btn{font-size:20px !important}
+
+
+</style> 
+
+
+
+</head>
+<body>
+
+
+
+
+
+
+
+<div class="wrap">
+	<div class="img_wr"><img src="img/mimg01.jpg" alt=""></div>
+	<div class="video_bg"></div>
+	<div class="img_wr video_wr">
+		<div class="tit"><img src="img/mimg02.png" alt=""></div>
+		<div class="video swiper-container">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide video1"><img src="https://img.youtube.com/vi/RzbhMf3XwNs/maxresdefault.jpg" alt=""></div>
+				<div class="swiper-slide video2"><img src="https://img.youtube.com/vi/JhQ88QO3MU4/maxresdefault.jpg" alt=""></div>
+				<div class="swiper-slide video3"><img src="https://img.youtube.com/vi/wRAq1QOrJOE/maxresdefault.jpg" alt=""></div>
+				<div class="swiper-slide video4"><img src="https://img.youtube.com/vi/sWrfJzAQ0Oo/maxresdefault.jpg" alt=""></div>
+				<div class="swiper-slide video5"><img src="https://img.youtube.com/vi/dKrC5Ns4VdA/maxresdefault.jpg" alt=""></div>
+				<div class="swiper-slide video6"><img src="https://img.youtube.com/vi/PCAIqwC3KTw/maxresdefault.jpg" alt=""></div>
+				<div class="swiper-slide video7"><img src="https://img.youtube.com/vi/2yuCN9qWQY4/maxresdefault.jpg" alt=""></div>
+				<div class="swiper-slide video8"><img src="https://img.youtube.com/vi/NX1C0Rsr1qE/maxresdefault.jpg" alt=""></div>
+			</div>
+		</div>
+		<div class="swiper-counter" id="numberSlides"></div>
+		<div class="swiper-button-prev ytb_btn_prev"></div>
+		<div class="swiper-button-next ytb_btn_next"></div>
+
+		<div class="video_pop">
+			<p class="video_close">CLOSE</p>
+			<iframe src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+
+	</div>
+
+	<div class="img_wr"><img src="img/mimg03.jpg" alt=""></div>
+
+	<div class="img_wr partner_wr">
+		<div class="tit"><img src="img/mimg04.png" alt=""></div>
+		<div class="partner">
+			<ul>
+				<li><img src="img/logo01_ov.png" alt=""></li>
+				<li><img src="img/logo02_ov.png" alt=""></li>
+				<li><img src="img/logo03_ov.png" alt=""></li>
+				<li><img src="img/logo04_ov.png" alt=""></li>
+				<li><img src="img/logo05_ov.png" alt=""></li>
+				<li><img src="img/logo06_ov.png" alt=""></li>
+				<li><img src="img/logo07_ov.png" alt=""></li>
+				<li><img src="img/logo08_ov.png" alt=""></li>
+				<li><img src="img/logo09_ov.png" alt=""></li>
+				<li><img src="img/logo10_ov.png" alt=""></li>
+				<li><img src="img/logo11_ov.png" alt=""></li>
+				<li><img src="img/logo12_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo13_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo14_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo15_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo16_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo17_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo18_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo19_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo20_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo21_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo22_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo23_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo24_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo25_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo26_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo27_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo28_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo29_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo30_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo31_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo32_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo33_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo34_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo35_ov.png" alt=""></li>
+				<li style="display: none;"><img src="img/logo36_ov.png" alt=""></li>
+			</ul>
+			<div class="partner_more">더 많은 파트너 보기</div> 
+		</div>
+	</div>
+	<div class="event5box">
+		<div class="tit"><img src="img/mimg04.png" alt=""></div>
+		<div class="event5wrap">
+			<form name="fwrite" id="fwrite" onsubmit="return submit1(this);" method="post" enctype="multipart/form-data" autocomplete="off">
+			<input type="hidden" name="w" value="<?php echo $w ?>">
+			<input type="hidden" name="qa_id" value="<?php echo $qa_id ?>">
+			<input type="hidden" name="sca" value="<?php echo $sca ?>">
+			<input type="hidden" name="stx" value="<?php echo $stx ?>">
+			<input type="hidden" name="page" value="<?php echo $page ?>">
+			<input type="hidden" name="qa_18" value="의료업">
+			<input type="hidden" name='qa_category' value="마케팅">
+			<input type='hidden' name='qa_12' value='랜딩'>
+			<input type='hidden' name='lancheck' value='1'>
+			<input type='hidden' name='qa_16' value='<?php echo $_SERVER[HTTP_REFERER];?>'>
+			<input type='hidden' name='lan_chk' value='<?php echo $_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI];?>'>
+			<table class='submit' cellspacing='0'>
+				<tr>
+					<th>업체명[병원명]</th>
+					<td><input type='text' class="inputr" title="업체명" name="qa_subject" id='qa_subject'></td>
+				</tr>
+				<tr>
+					<th>예 산</th>
+					<td><input type='text' class="inputr" title="예산" name="qa_4" id='qa_4'></td>
+				</tr>
+				<tr>
+					<th>연락처</th>
+					<td><input type='text' class="inputr" title="연락처" name="qa_hp" id='qa_hp' placeholder="- 없이 입력해주세요."></td>
+				</tr>
+				<tr>
+					<th style="height:130px">내 용</th>
+					<td><input type='text' class="inputr" title="내용" name="qa_content" id='qa_content' style="height:130px"></td>
+				</tr>				
+				<tr class="info_wr">
+					<td class="info" colspan="2"><input type="checkbox" id='agree' value="" checked /><label for='agree'> 개인정보취급방침에 동의합니다.</label></td>
+				</tr>
+			</table>
+			<input type="button" value="상담신청 바로가기" onclick='submit1();' class="input_btn" />
+		</div>
+	</div>
+</div>
+
+<script>
+$(function(){
+	// youtube
+	var ytbSlide = new Swiper ('.video', {
+		autoplay: false,
+		slidesPerView: 'auto',
+		loop : true,
+		runCallbacksOnInit : false,
+		onInit: function(e){
+			var realIndex = e.slides.eq(e.activeIndex).attr('data-swiper-slide-index');
+			var num = parseInt(realIndex);
+			var offer = document.querySelector('#numberSlides');
+			offer.innerHTML = (num+1) + '<span> <i>/ </i> 8 </span>' ;
+		},
+		onSlideChangeEnd: function(e){
+			var realIndex = e.slides.eq(e.activeIndex).attr('data-swiper-slide-index');
+			var num = parseInt(realIndex);
+			var offer = document.querySelector('#numberSlides');
+			offer.innerHTML = num+1 + '<span> <i>/ </i> 8 </span>';
+		},
+		nextButton: '.ytb_btn_next',
+		prevButton: '.ytb_btn_prev',	
+	}); 
+
+	$('.swiper-slide').click(function(){
+		$('.video_bg').show();
+		$('.video_pop').show();
+	});
+	$('.video1').click(function(){$('.video_pop').children('iframe').attr('src','https://www.youtube.com/embed/RzbhMf3XwNs?controls=1&autoplay=1');});
+	$('.video2').click(function(){$('.video_pop').children('iframe').attr('src','https://www.youtube.com/embed/JhQ88QO3MU4?controls=1&autoplay=1');});
+	$('.video3').click(function(){$('.video_pop').children('iframe').attr('src','https://www.youtube.com/embed/wRAq1QOrJOE?controls=1&autoplay=1');});
+	$('.video4').click(function(){$('.video_pop').children('iframe').attr('src','https://www.youtube.com/embed/sWrfJzAQ0Oo?controls=1&autoplay=1');});
+	$('.video5').click(function(){$('.video_pop').children('iframe').attr('src','https://www.youtube.com/embed/dKrC5Ns4VdA?controls=1&autoplay=1');});
+	$('.video6').click(function(){$('.video_pop').children('iframe').attr('src','https://www.youtube.com/embed/PCAIqwC3KTw?controls=1&autoplay=1');});
+	$('.video7').click(function(){$('.video_pop').children('iframe').attr('src','https://www.youtube.com/embed/2yuCN9qWQY4?controls=1&autoplay=1');});
+	$('.video8').click(function(){$('.video_pop').children('iframe').attr('src','https://www.youtube.com/embed/NX1C0Rsr1qE?controls=1&autoplay=1');});
+	$('.video_close').click(function(){
+		$('.video_bg').hide();
+		$('.video_pop').hide();
+		$('.video_pop').children('iframe').attr('src','');
+	});
+
+	// partner
+	$('.partner_more').click(function(e){
+		$(".partner ul li:hidden").slice(0,3).fadeIn();
+		if ($(".partner ul li:hidden").length < 1) $(this).fadeOut();
+  });
+
+}); 
+</script>
+
+<script>
+function submit1()
+{
+
+	form = document.fwrite;
+
+
+	if(form.qa_subject.value=="")
+	{
+		alert("업체명을 입력해주세요.");
+		form.qa_subject.focus();
+		return;
+	}
+	if(form.qa_4.value=="")
+	{
+		alert("예산을 입력해주세요.");
+		form.qa_4.focus();
+		return;
+	}
+	if(form.qa_hp.value=="")
+	{
+		alert("연락처를 입력해주세요.");
+		form.qa_hp.focus();
+		return;
+	}
+
+	if(form.qa_content.value=="")
+	{
+		alert("문의내용을 입력해주세요.");
+		form.qa_content.focus();
+		return;
+	}
+
+	 if (!form.agree.checked)
+	{
+		alert("개인정보취급방침에 동의하셔야됩니다.");
+		form.agree.focus();
+        return; 
+	}
+
+	form.action ='http://bmmost.net/bbs/qawrite_update.php';
+	form.submit();
+}
+</script>
+
+
+</body>
+</html>
+
+
+
